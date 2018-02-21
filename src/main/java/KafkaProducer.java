4031b14b20415b0c0b53857415c1e9a46832dc82
Arun <arun.kasireddi@gmail.com>
2018-02-21 22:18:34 +0000

    Added Producer: Send message to topic

--- src/main/java/KafkaProducer.java
+++ src/main/java/KafkaProducer.java
@@ -1,18 +1,33 @@
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
 import java.util.Properties;
 
 public class KafkaProducer {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         Properties properties = new Properties();
 
         // kafka bootstrap server
         properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
-        properties.setProperty("key.serializer", "");
-        properties.setProperty("value.serializer", "");
-
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", StringSerializer.class.getName());
+        properties.setProperty("linger.ms", "1");
 
         // kafka producer acks
+        properties.setProperty("acks", "1");
+        properties.setProperty("retries", "3");
 
         // topic name
+        Producer<String, String> producer = new org.apache.kafka.clients.producer.KafkaProducer<String, String>(properties);
+
+        for(int key = 0; key < 10; key++){
+            ProducerRecord<String, String> producerRecord =
+                    new ProducerRecord<String, String>("second_topic", Integer.toString(key), "message that has key " + Integer.toString(key));
+
+            producer.send(producerRecord);
+        }
+        producer.close();
     }
 
 }
